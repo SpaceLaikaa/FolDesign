e83b0883e8091f8db17b272ff40356e63270e6e0
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-17 15:28:38 +0300

    Add FileHandler abstract class and implement copyFile method

--- src/main/java/com/foldesign/handler/FileHandler.java
+++ src/main/java/com/foldesign/handler/FileHandler.java
@@ -1,7 +1,29 @@
 package com.foldesign.handler;
 
+import java.io.*;
 import java.nio.file.Path;
 
 public abstract class FileHandler {
+    private Path folderPath;
+
+    public FileHandler(Path folderPath){
+        this.folderPath=folderPath;
+    }
+
+    public Path getFolderPath() {return folderPath;}
+
     public abstract void handle(Path filePath);
+
+    public void copyFile(Path mainFolder, Path targetFolder){
+        try(InputStream inputS = new FileInputStream(mainFolder.toFile());
+             OutputStream outputS = new FileOutputStream(targetFolder.toFile())){ //toFile bc stream is an old java thing must turn path to file
+
+
+        }
+          catch (FileNotFoundException e) {
+            System.out.println("File Not Found: "+e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
