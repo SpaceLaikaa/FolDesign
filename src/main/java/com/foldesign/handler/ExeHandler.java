dd52413e19ade5f3f507a4e4a47ef569c80144a0
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2026-05-17 23:06:18 +0300

    feat: implement FolderHandler orchestrator with factory map

---
+++ src/main/java/com/foldesign/handler/ExeHandler.java
@@ -0,0 +1,14 @@
+package com.foldesign.handler;
+
+import java.nio.file.Path;
+
+public class ExeHandler extends FileHandler {
+    public ExeHandler(Path folderPath) {
+        super(folderPath);
+    }
+
+    @Override
+    public void handle(Path filePath) {
+
+    }
+}
--- src/main/java/com/foldesign/manager/FolderHandler.java
+++ src/main/java/com/foldesign/manager/FolderHandler.java
@@ -1,13 +1,34 @@
 package com.foldesign.manager;
 
-import com.foldesign.handler.FileHandler;
+import com.foldesign.handler.*;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.HashMap;
 
 public class FolderHandler {
-    File myDownloadsFolder = new File("C:/Users/ardaa/İndirilenler");
-    File[] downloadsFiles = myDownloadsFolder.listFiles();
+    private HashMap<String, FileHandler> factory;
 
-    HashMap<String, FileHandler> factory = new HashMap<>();
+    public FolderHandler(){
+        this.factory = new HashMap<>();
+
+        factory.put("jpeg" , new JpegHandler(Paths.get("İndirilenler/Jpg_Folder")));
+        factory.put("jpg" , new JpegHandler(Paths.get("İndirilenler/Jpg_Folder")));
+        factory.put("png", new PngHandler(Paths.get("İndirilenler/Png_Folder")));
+        factory.put("pdf", new PdfHandler(Paths.get("İndirilenler/Pdf_Folder")));
+        factory.put("exe", new ExeHandler(Paths.get("İndirilenler/Pdf_Folder")));
+    }
+    public void scanAndOrganise(){
+        File myDownloadsFolder = new File("C:/Users/ardaa/İndirilenler");
+        File[] downloadsFiles = myDownloadsFolder.listFiles();
+
+        if(downloadsFiles == null){System.out.println("Path Error: Null"); return;}
+
+        for(File file : downloadsFiles){
+            if(file.isFile()){
+                String fileName = file.getName().toLowerCase();
+
+            }
+        }
+    }
 }
